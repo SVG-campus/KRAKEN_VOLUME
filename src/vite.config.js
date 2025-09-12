9010be899cd32cf9950deedd2900a74e4ca9a952
Santiago Villalobos-Gonzalez <santiago@laudatecorpus.onmicrosoft.com>
2025-09-12 14:34:27 -0700

    COMMIT

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,15 +1,8 @@
-import path from "path"
-import { fileURLToPath } from "url"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
   server: {
     port: 3001, // Run dev server on a different port than the backend
   },
