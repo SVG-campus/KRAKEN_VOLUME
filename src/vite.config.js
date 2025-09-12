184869357fa50ab9f6834055a55638b85ab8f7aa
Santiago Villalobos-Gonzalez <santiago@laudatecorpus.onmicrosoft.com>
2025-09-12 14:46:51 -0700

    COMMIT

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,8 +1,18 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
   server: {
     port: 3001, // Run dev server on a different port than the backend
   },
