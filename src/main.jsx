e756f22d3400040cb1817c40d6969b040aaecd65
Santiago Villalobos-Gonzalez <santiago@laudatecorpus.onmicrosoft.com>
2025-09-12 18:48:54 -0700

    COMMIT

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-const root = document.getElementById("root");
-createRoot(root).render(
+let mount =
+  document.getElementById('root') ||
+  document.getElementById('app');
+
+if (!mount) {
+  mount = document.createElement('div');
+  mount.id = 'root';
+  document.body.appendChild(mount);
+}
+
+createRoot(mount).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
